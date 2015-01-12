3300fc5fd2828dad9a59c76db8e428c6afc27d5d
Jeremy Saenz <jeremy.saenz@gmail.com>
2015-01-11 18:53:12 -0800

    Adding a benchmark

---
+++ benchmark_test.go
@@ -0,0 +1,23 @@
+package mix_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/codegangsta/mix"
+)
+
+func Benchmark1Route(b *testing.B) {
+
+	m := mix.New()
+	m.Get("/pages/:pageId/events/:id", func(http.ResponseWriter, *http.Request) {
+	})
+
+	req, _ := http.NewRequest("GET", "/pages/123/events/456", nil)
+	res := httptest.NewRecorder()
+
+	for i := 0; i < b.N; i++ {
+		m.ServeHTTP(res, req)
+	}
+}
